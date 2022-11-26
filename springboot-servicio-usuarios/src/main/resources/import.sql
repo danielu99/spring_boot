e2569e664ac6ea132416000f74bb9f863618aa7e
danielu99 <danielulc99@outlook.com>
2022-11-25 22:31:25 -0600

    postresql configurations

--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios(username,password,enabled,nombre,apellido,email) VALUES('daniel','$2a$10$QikgNmW8NKRa1g4M67vLP.Nk8TZTClaJ6fH4pF6VAMupeRR/13GjK',1,'Daniel','Leon','daniel@correo.com');
-INSERT INTO usuarios(username,password,enabled,nombre,apellido,email) VALUES('admin','$2a$10$u1wOJDlpuJlegvaZcD32/usdQO08e45fwvobICGuyG6q34XxFlo/i',1,'John','Doe','john.doe@bolsadeideas.com');
+INSERT INTO usuarios(username,password,enabled,nombre,apellido,email) VALUES('daniel','$2a$10$QikgNmW8NKRa1g4M67vLP.Nk8TZTClaJ6fH4pF6VAMupeRR/13GjK',true,'Daniel','Leon','daniel@correo.com');
+INSERT INTO usuarios(username,password,enabled,nombre,apellido,email) VALUES('admin','$2a$10$u1wOJDlpuJlegvaZcD32/usdQO08e45fwvobICGuyG6q34XxFlo/i',true,'John','Doe','john.doe@bolsadeideas.com');
 
 INSERT INTO roles(nombre) VALUES('ROLE_USER');
 INSERT INTO roles(nombre) VALUES('ROLE_ADMIN');
